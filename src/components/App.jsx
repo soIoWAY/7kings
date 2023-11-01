5aabaf7b99aa7f29df78a0ad9de06ba8f48ef2c7
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2023-11-01 22:26:48 +0200

    navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
 function App() {
 
   return (
     <>
       <Header />
+      <Navbar />
     </>
   )
 }
