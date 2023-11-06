a436395e1eda250170ae47e9c3d83a7793a9afc5
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2023-11-06 16:41:03 +0200

    review

---
+++ src/components/games/gamesLogic/cherrySlot/interface/checker.js
@@ -0,0 +1,33 @@
+export const checker = (isGameStarted, fruits) => {
+    if (isGameStarted) {
+        if (fruits.every(fruit => fruit === '🍒')) {
+            console.log('x30')
+        } else if ((fruits[0] === '🍒' && fruits[1] === fruits[0] && fruits[2] === '💰') || (fruits[0] === '💰' && fruits[1] === '🍒' && fruits[2] === fruits[1])) {
+            console.log('x6')
+        } else if ((fruits[0] === '🍒' && fruits[1] === fruits[0] && fruits[2] === '💣') || (fruits[0] === '💣' && fruits[1] === '🍒' && fruits[2] === fruits[1])) {
+            console.log('x0')
+        } else if (fruits.every(fruit => fruit === '🍇')) {
+            console.log('x20')
+        } else if ((fruits[0] === '🍇' && fruits[1] === fruits[0] && fruits[2] === '💰') || (fruits[0] === '💰' && fruits[1] === '🍇' && fruits[2] === fruits[1])) {
+            console.log('x4')
+        } else if ((fruits[0] === '🍇' && fruits[1] === fruits[0] && fruits[2] === '💣') || (fruits[0] === '💣' && fruits[1] === '🍇' && fruits[2] === fruits[1])) {
+            console.log('x0')
+        } else if (fruits.every(fruit => fruit === '🍋')) {
+            console.log('x20')
+        } else if ((fruits[0] === '🍋' && fruits[1] === fruits[0] && fruits[2] === '💰') || (fruits[0] === '💰' && fruits[1] === '🍋' && fruits[2] === fruits[1])) {
+            console.log('x4')
+        } else if ((fruits[0] === '🍋' && fruits[1] === fruits[0] && fruits[2] === '💣') || (fruits[0] === '💣' && fruits[1] === '🍋' && fruits[2] === fruits[1])) {
+            console.log('x0')
+        } else if (fruits.every(fruit => fruit === '🍏')) {
+            console.log('x10')
+        } else if ((fruits[0] === '🍏' && fruits[1] === fruits[0] && fruits[2] === '💰') || (fruits[0] === '💰' && fruits[1] === '🍏' && fruits[2] === fruits[1])) {
+            console.log('x3')
+        } else if ((fruits[0] === '🍏' && fruits[1] === fruits[0] && fruits[2] === '💣') || (fruits[0] === '💣' && fruits[1] === '🍏' && fruits[2] === fruits[1])) {
+            console.log('x0')
+        } else if (fruits.every(fruit => fruit === '💰')) {
+            console.log('x5')
+        } else if (fruits.every(fruit => fruit !== '💣' && fruit !== '💰') && (fruits[0] === fruits[1] || fruits[1] === fruits[2])) {
+            console.log('x2')
+        }
+    }
+}
